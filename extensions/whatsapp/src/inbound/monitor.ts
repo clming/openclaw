a6e3dee96b9176894e4c436acdcb5c2a88d23ecc
Joey Krug <joeykrug@gmail.com>
2026-03-14 21:47:50 -0400

    Auto-reply: harden inbound debounce restart flushing

--- extensions/whatsapp/src/inbound/monitor.ts
+++ extensions/whatsapp/src/inbound/monitor.ts
@@ -521,6 +521,7 @@ export async function monitorWebInbox(options: {
   return {
     close: async () => {
       try {
+        debouncer.unregister();
         detachMessagesUpsert();
         detachConnectionUpdate();
         closeInboundMonitorSocket(sock);
